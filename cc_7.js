bf85e0612fdbe715c1fe09dd058cd860fa283a67
Annabelle Fait <imannaf8@gmail.com>
2025-02-15 18:14:39 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -18,3 +18,18 @@ function calculateHourlyWage(salary, hoursPerWeek) { // Creates a function to ca
 
 calculateHourlyWage(52000, 40); // Calculates the hourly wage of an employee with a $52000 salary that works 40 hours per week
 calculateHourlyWage(75000, 35); // Calculates the hourly wage of an employee with a $75000 salary that works 35 hours per week
+
+
+// Task 3 - Customer Loyalty Discount
+
+const calculateLoyaltyDiscount = (amount, years) => { // Creates an arrow function to calculate the loyalty discount based on the number of years
+    let discount = 0
+    if (years >= 5) discount = amount * .15;
+    else if (years >= 3) discount = amount * .10;
+    else discount = amount * .05;
+// This function gives a 15% discount to customers of 5 years or more, 10% to 3 years or more, and 5% to three years or less
+    console.log(`Discounted Price: $${(amount - discount).toFixed(2)}`);
+} // Calculates the discounted price by subtracting the amount-discount and rounds to two decimal places
+
+calculateLoyaltyDiscount(100, 6); // Calculates the Discounted price using the function if price is 100 and years is 6
+calculateLoyaltyDiscount(200, 2); // Calculates the Discounted price using the function if price is 200 and years is 2
